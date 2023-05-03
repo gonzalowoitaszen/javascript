c7aa73a1cafedf30e377192b8b41a0b0410616e8
Gonzalo <gonzalow20@gmail.com>
2023-05-03 19:41:28 -0300

    segundaentrega

--- js/inicio.js
+++ js/inicio.js
@@ -1,7 +1,9 @@
-let ingresarNombre = prompt("Ingresa tu nombre: ");
-alert("hola " + ingresarNombre + "" +", " + "Debes ejecutar en la consola js: \n preguntarPrecios()");
+const ingresarNombre = prompt("Ingresa tu nombre: ");
+alert("hola " + ingresarNombre);
 
+preguntarPrecios()
 
+function preguntarPrecios() {
 let valor = ""
 const mensajeDeBienvenida = "Ingrese el código de la planta que desea comprar: \n " +
                             "a) auto-cinderella-jack \n" +
@@ -10,42 +12,58 @@ const mensajeDeBienvenida = "Ingrese el código de la planta que desea comprar: 
                             "d) auto-skywalker-haze \n"
 
 
-function preguntarPrecios() {
     let selection = prompt(mensajeDeBienvenida).toLowerCase()
 
-
-    if (selection !== "a" && selection !== "b" && selection !== "c" && selection !== "d")
-     { 
-      alert ("debes ingresar un código valido")
-    }
-
-    else{
-
-    
-    switch (selection) {
-        case "a":
-            valor = "auto cinderella jack cuesta $10000"
-            break
-        case "b":
-            valor = "auto lemon kix cuesta $11000"
-            break
-        case "c":
-            valor = "auto orange bud cuesta $12000"
-            break
-        case "d":
-            valor = "auto skywalker haze cuesta $13000"
-            break
-        default:
+    if (selection !== "a" && selection !== "b" && selection !== "c" && selection !== "d") { 
+        alert ("Debes ingresar un código válido")
+    } else {
+        switch (selection) {
+            case "a":
+                valor = "auto cinderella jack cuesta $10000"
+                break
+            case "b":
+                valor = "auto lemon kix cuesta $11000"
+                break
+            case "c":
+                valor = "auto orange bud cuesta $12000"
+                break
+            case "d":
+                valor = "auto skywalker haze cuesta $13000"
+                break
+            default:
         }
         alert(valor)
     }   
-    const respuesta = confirm("Quieres saber el precio de otra planta?")
 
+    const respuesta = confirm("¿Quieres saber el precio de otra planta?")
 
-    while(respuesta) {
+    if (respuesta) {
         preguntarPrecios()
-
-    }
-
-
+    } 
 }
+
+const plantas = [
+    { nombre: "auto cinderella jack", 
+      precio: 10000 },
+    { nombre: "auto lemon kix", 
+      precio: 11000 },
+    { nombre: "auto orange bud", 
+      precio: 12000 },
+    { nombre: "auto skywalker haze", 
+      precio: 13000 }
+];
+
+
+const nuevaPlanta = {
+    nombre: "auto amnesia haze", 
+    precio: 14000 };
+    plantas.push(nuevaPlanta);
+    console.log(nuevaPlanta.nombre)
+    
+    
+    const nombresPlantas = plantas.map(planta => planta.nombre);
+    console.log(nombresPlantas)
+
+    const PrecioPlantas = plantas.map(planta => planta.precio);
+    console.log(PrecioPlantas)
+
---
+++ js/inicio2.js
@@ -0,0 +1,17 @@
+
+
+// let numero1 = Number (prompt("ingrese un numero: "));
+// if (numero1 > 1000){
+//     alert("el numero es mayor que 1000")
+
+// } else{
+//     alert("el numero es menor que 1000")
+// }
+
+
+let palabra = prompt("ingrese una palabra");
+if (palabra == "hola"){
+    alert("la palabra ingresada fue hola")
+} else{
+    alert("la palabra ingresada no es valida")
+}
